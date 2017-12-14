019b81ed5cd3be9a563923cd4bbf53060816220a
Luiz Tanure <letanure@gmail.com>
2017-12-14 01:45:54 +0100

    demo

---
+++ src/components/ChildSelects.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="childSelects">
+    <select name="" id="">
+      <option value="">childSelects 1</option>
+      <option value="">childSelects 2</option>
+    </select>
+    <select name="" id="">
+      <option value="">childSelects 3</option>
+      <option value="">childSelects 4</option>
+    </select>
+    <button @click="remove()">
+      remove item child
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'childSelects',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    remove () {
+      this.$emit('remove')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/ListSelects.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="childSelects">
+    <h1>List selects</h1>
+    <template v-for="(item, index) in lista">
+      <div>
+        <span>
+          {{ item.label }}
+        </span>
+        <child-selects @remove="removeOnParent(index)"></child-selects>
+      </div>
+    </template>
+    <button @click="addItem()" >
+      Add item child
+    </button>
+  </div>
+</template>
+
+<script>
+import ChildSelects from './ChildSelects'
+
+export default {
+  name: 'ListSelects',
+  data () {
+    return {
+      lista: [],
+      count: 0
+    }
+  },
+  methods: {
+    addItem () {
+      this.count++
+      this.lista.push({
+        label: `item ${this.count}`
+      })
+    },
+    removeOnParent (index) {
+      this.$delete(this.lista, index)
+    }
+  },
+  components: {
+    ChildSelects
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
